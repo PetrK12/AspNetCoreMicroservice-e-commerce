7fe3735458c44060eaa33ccfd6a15bd6aad1d51a
Petrk12 <petrkutil123@gmail.com>
2023-03-24 23:52:54 +0100

    Basket API implementation

---
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿using Basket.API.Models;
+
+namespace Basket.API.Data
+{
+    public interface IBasketRepository
+    {
+        public Task<ShoppingCart> GetBasket(string userName);
+
+        public Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+
+        public Task DeleteBasket(string userName);
+    }
+}
+
