62105e46aba3e26ee52c5f50e68e2fce3f0cf86c
Petrk12 <petrkutil123@gmail.com>
2023-03-28 23:50:19 +0200

    Initial implementation of OcelotApiGateway

---
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+await app.UseOcelot();
+
+app.Run();
+
