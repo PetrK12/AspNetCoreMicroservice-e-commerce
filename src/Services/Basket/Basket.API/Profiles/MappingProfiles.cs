495f87f2735e2277138e08e411633874e170d2c9
Petrk12 <petrkutil123@gmail.com>
2023-03-28 19:33:21 +0200

    Implementation of publishing checkout msg in Basket.API with MassTransit

---
+++ src/Services/Basket/Basket.API/Profiles/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+using Basket.API.Models;
+using EventBus.Messages.Events;
+
+namespace Basket.API.Profiles
+{
+	public class MappingProfiles : Profile
+	{
+		public MappingProfiles()
+		{
+            CreateMap<BasketCheckoutEvent, BasketCheckout>().ReverseMap();
+        }
+	}
+}
+
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,24 +3,48 @@ using Basket.API.GrpcServices;
 using Discount.Grpc.Protos;
 using Grpc.Net.Client;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using MassTransit;
+using Basket.API.Profiles;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddStackExchangeRedisCache(options => options.Configuration =
-    builder.Configuration.GetValue<string>("CacheSettings:ConnectionString"));
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
-builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
-    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
-
-
-builder.Services.AddScoped<DiscountGrpcService>();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Basket.API", Version = "v1" });
+});
+//builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile<MappingProfiles>();
+});
+
+//Redis config
+builder.Services.AddStackExchangeRedisCache(options => options.Configuration =
+    builder.Configuration.GetValue<string>("CacheSettings:ConnectionString"));
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+//Grpc
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+builder.Services.AddScoped<DiscountGrpcService>();
+
+//MassTransit-RabbitMQ
+builder.Services.AddMassTransit(configure =>
+{
+    configure.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMqHost"]);
+    });
+});
+
+builder.Services.Configure<MassTransitHostOptions>(options =>
+{
+    options.WaitUntilStarted = true;
+    options.StartTimeout = TimeSpan.FromSeconds(30);
+    options.StopTimeout = TimeSpan.FromMinutes(1);
 });
 
 var app = builder.Build();
